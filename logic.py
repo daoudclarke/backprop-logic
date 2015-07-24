1d46eadd1e6e39bd5ee43a69525d70c3dad0aec5
Daoud Clarke <daoud.clarke@gmail.com>
2015-07-24 15:26:26 +0100

    Initial commit

---
+++ logic.py
@@ -0,0 +1,15 @@
+"""
+Perform backpropagation for model building in fuzzy-style logics.
+
+Variables are fixed dimensional vectors.
+"""
+
+# Bill runs
+# exists(x, bill(x) and run(x))
+
+class Max(object):
+    def __init__(self, parents):
+        self.parents = parents
+
+    def forward(inputs):
+        return
